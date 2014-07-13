3a4583886944a53a4a328cc21f219f12bb9cefe9
carloscampos2014 <carlosalbertode.campos@gmail.com>
2014-07-13 18:22:38 -0300

    Nova Atualização

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quiron.LojaVirtual.Dominio.Entidade;
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    class ProdutosRepositorio
+    {
+        private readonly EfDbContext _context = new EfDbContext();
+        public IEnumerable<Produto> Produtos
+        {
+            get { return _context.Produtos; }
+        }
+    }
+}
